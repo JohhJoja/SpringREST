72fc3143557b703d9519306803ba5e821a5da42b
JohhJoja <08022002dimaeliseew@gmail.com>
2025-04-24 17:07:32 +0300

    create controller, dao and model

---
+++ src/main/java/com/eliseew/dima/controllers/PeopleController.java
@@ -0,0 +1,34 @@
+package com.eliseew.dima.controllers;
+
+import com.eliseew.dima.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model){
+        // Получим всех людей из DAO и передадим на отображение в представление
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model){
+        // Получим одного чела по айди из ДАО и передадим на отображение в представление
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+}
---
+++ src/main/java/com/eliseew/dima/dao/PersonDAO.java
@@ -0,0 +1,28 @@
+package com.eliseew.dima.dao;
+
+import com.eliseew.dima.model.People;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT = 0;
+    private List<People> people;
+    {
+        people = new ArrayList<>();
+        people.add(new People("Dima", ++PEOPLE_COUNT));
+        people.add(new People("Dimasik", ++PEOPLE_COUNT));
+        people.add(new People("Dimachlenus", ++PEOPLE_COUNT));
+        people.add(new People("Ananas", ++PEOPLE_COUNT));
+        people.add(new People("Ananasik", ++PEOPLE_COUNT));
+        people.add(new People("Ananachlenus", ++PEOPLE_COUNT));
+    }
+    public List<People> index(){
+        return people;
+    }
+    public People show(int id){
+        return people.get(id);
+    }
+}
